cf080f4e91e0c30825694191035f977590925cc7
Suryasnsh Sah <suryanshsahs@gmail.com>
2026-01-08 11:38:43 +0530

    update

--- triangle.c
+++ triangle.c
@@ -1,3 +1,14 @@
-//
-// Created by Asus on 1/8/2026.
-//
+#include <stdio.h>
+
+int main() {
+    int n = 4;
+
+    for(int i = 1; i <= n; i++) {
+        for(int j = 1; j <= i; j++) {
+            printf("* ");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
